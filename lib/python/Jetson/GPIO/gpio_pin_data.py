f52df050f5d922013275a5f168ab2133763ccd4d
mdeniz0 <mehmet@forecr.io>
2026-03-26 15:53:30 +0300

    Latest fixes included from the base repo (with latest commits on Feb 25, 2026)

--- lib/python/Jetson/GPIO/gpio_pin_data.py
+++ lib/python/Jetson/GPIO/gpio_pin_data.py
@@ -257,8 +257,11 @@ compats_jetson_orins_nano = (
     "nvidia,p3509-0000+p3767-0005",
     "nvidia,p3768-0000+p3767-0005",
     "nvidia,p3768-0000+p3767-0005-super",
+    "nvidia,p3509-0000+p3767-0005-super",
     "nvidia,p3768-0000+p3767-0003-super",
+    "nvidia,p3509-0000+p3767-0003-super",
     "nvidia,p3768-0000+p3767-0004-super",
+    "nvidia,p3509-0000+p3767-0004-super",
 )
 
 JETSON_ORIN_PIN_DEFS = [
@@ -799,6 +802,7 @@ compats_jetson_thor_reference = (
     'nvidia,p3971-0050+p3834-0005',
     'nvidia,p3971-0080+p3834-0008',
     'nvidia,p3971-0089+p3834-0008',
+    'nvidia,p4071-0000+p3834-0008',
 )
 
 
@@ -1060,7 +1064,7 @@ def get_model():
             warn_if_not_carrier_board('3509', '3768')
             return JETSON_ORIN_NANO
         elif matches(compats_jetson_thor_reference):
-            warn_if_not_carrier_board('3971')
+            warn_if_not_carrier_board('3971', '4071')
             return JETSON_THOR_REFERENCE
 
     # get model info from the environment variables for docker containers
